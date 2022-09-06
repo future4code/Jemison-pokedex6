49691df91a3f08abd477b8fc5ace73bdf86ccdba
Juan Carlos Torterolo Pedroza <juan.torterolo@gmail.com>
2022-09-05 22:19:30 -0300

    Master atualizada
    junção Juan e Jenni

--- pokedex-labenu/src/Components/Home.jsx
+++ pokedex-labenu/src/Components/Home.jsx
@@ -2,19 +2,13 @@ import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { goToPokedex } from './../Route/Coordinator';
 import PokeCard from './PokeCard';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, CardsHome, Header } from '../Styles/StyleDetails'
-import pokemons from './../Styles/img/pokemon.webp'
-
+// import { Container, CardsHome, Header } from '../Styles/StyleDetails'
 import { DivPai, CardsHome, Bar, ButtonsHome } from '../Styles/StyleDetails'
-import axios from 'axios';
 
 function Home() {
 
     const navigate = useNavigate()
-    const [listPokemons, setListPokemons] = useState([])
-
     const [listPokemons, setListPokemons] = useState([])
     const [nextPage, setNextPage] = useState("")
     const [previousPage, setPreviousPage] = useState("")
